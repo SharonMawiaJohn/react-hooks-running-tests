a2a56d3d1d7047c0b71a8cdcd54fb603006225b7
SharonMawiaJohn <sharonmawia76@gmail.com>
2024-07-30 08:32:06 +0300

    Add the changes

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,20 @@
+// import React from "react";
+
+// function Article() {
+//   return (
+//     <div>
+//       {/* please pass this test */}
+//     </div>
+//   );
+// }
+
+// export default Article;
 import React from "react";
 
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      <p>please pass this test</p> {/* Change this line */}
     </div>
   );
 }
